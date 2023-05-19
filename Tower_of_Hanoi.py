36bcb08a80796bcc6148581429f072747f3161d2
AustinA86e <austinmasila@gmail.com>
2023-05-19 07:58:26 +0530

    First-commit

---
+++ Tower_of_Hanoi.py
@@ -0,0 +1,9 @@
+def tower_of_hanoi(disks,source,auxiliary,target):
+    if (disks == 1):
+        print('move disks 1 from rod {} to rod {}.'.format(source,target))
+        return
+    tower_of_hanoi(disks - 1,source,target,auxiliary)
+    print('move disks {} from rod {} to rod {}.'.format(disks,source,target))
+    tower_of_hanoi(disks - 1,auxiliary,source,target)
+disks = int(input('Enter the number of disks :'))
+tower_of_hanoi(disks,'A','B','C')
